f47702bdaf333abddeeb78bd50b958148dbc1f2c
DS <DS@SIST233>
2017-12-12 14:26:19 +0900

    first commit

---
+++ MVCProject2/src/com/sist/model/ContentModel.java
@@ -0,0 +1,13 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ContentModel implements Model {
+
+	@Override
+	public String excute(HttpServletRequest req) throws Exception {
+
+		return null;
+	}
+
+}
---
+++ MVCProject2/src/com/sist/model/ListModel.java
@@ -0,0 +1,13 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ListModel implements Model {
+
+	@Override
+	public String excute(HttpServletRequest req) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
